a4ec119aa830bf6281f9a42487899599fa094af3
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-06-25 16:24:40 -0500

    a

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   if (props.results) {
   return (
     <div className="Results">
-      Hi
+      <h2>{props.results.word}</h2>
+      {props.results.meanings.map(function(meaning, index) {
+        return <div key={index}>
+          <Meaning meaning={meaning} />
+        </div>
+      })}
     </div>
   )
   } else {
