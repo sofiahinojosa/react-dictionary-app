d277d22043d00815f94543e5f7cf941ca00e68cb
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2021-06-26 18:53:36 -0500

    a

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css"
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] =useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+   function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,6 +22,12 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    const pexelsApiKey = "563492ad6f917000010000016931941ce46e41a2974474b90c94bc76";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    axios.get(pexelsApiUrl, {
+      headers: { Authorization: `Bearer ${pexelsApiKey}` },
+    }).then(handlePexelsResponse)
   }
 
   function handleKeywordChange(event) {
@@ -31,6 +43,7 @@ export default function Dictionary() {
       <small>Coded by <a href="https://www.instagram.com/sofiahinojosadl_/">Sofia Hinojosa</a> and is open-sourced in <a href="https://github.com/sofiahinojosa/react-dictionary-app">GitHub</a></small>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
